8e9250693148306c071c341e57277a03a4fb63f6
bravejang <wkdrmfmt@gmail.com>
2021-03-18 10:56:29 +0900

    Building the REST API

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "rooms"
 
 urlpatterns = [
-    path("list/", views.ListRoomsView.as_view()),
-    path("<int:pk>", views.SeeRoomView.as_view()),
+    path("", views.RoomsView.as_view()),
+    path("search/", views.room_search),
+    path("<int:pk>/", views.RoomView.as_view()),
 ]
